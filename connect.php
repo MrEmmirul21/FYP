bc7cd1f64c29d62cba3f5a21cfd477d0de06b2fa
Emmirul Haziq <92413479+MrEmmirul21@users.noreply.github.com>
2023-02-22 05:36:01 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,8 @@
 <?php
+	//$connection = mysqli_connect(hostname,username,password,database_name);
 	$connection = mysqli_connect("localhost","root","","waterquality");
 
 	if (!$connection)
 		die('MySQL ERROR: ' . mysqli_connect_error());
 	
-	else 
-		echo "Database connection succesfully <br/>";
 ?>
